94b075daa97330e611414167be426065e31b9232
Varyag3 <maxk7074@gmail.com>
2019-03-01 16:07:11 +0300

    test

--- Snake/js/game.js
+++ Snake/js/game.js
@@ -1,8 +1,13 @@
-var snake;
-var scl = 20;
+var scl = 10;
 var app;
-var food;
-var tail =[];
+var arr = new Array();
+var head;
+var foodx;
+var foody;
+var total = 0;
+var tailArr= [];
+var tailSegment;
+var s;
 var init = function () {
     var loader = PIXI.loader
         .add('images/bg.png')
@@ -14,110 +19,121 @@ var init = function () {
         const canvas = document.getElementById('mycanvas');
         app = new PIXI.Application({
             view: canvas,
-            width: window.innerWidth,eight: window.innerHeight
+            width: window.innerWidth,
+            height: window.innerHeight
         });
-        snake.init();
         app.renderer.autoResize = true;
         app.renderer.resize(window.innerWidth, window.innerHeight);
-        app.ticker.FPS=10;
-        app.ticker.add(function (delta) {
-           snake.update();
-        if(snake.eat(food)){
-            snake.total++;
-            app.stage.removeChild(food);
-            placemeal();
-            snake.addTail();
-        };
-        // snake.show();
-        })
-        placemeal();
-        window.addEventListener('keydown', handler);
-        window.addEventListener('resize', onResize);
-    }
 
+        creatMatrix();
+        S = new SnakeSegment()
+        tailArr.push(s)
+        var counter = 0;
+        app.ticker.add(function (delta) {
+        window.addEventListener('keydown', handler);
+            counter++
+          if(counter>=10){
+              S.goTo();
+              counter=0;
+            }
+        });
+        placemeal();
+    }
 }
 init();
-snake = new Snake();
-function Snake (){
-    const rect = new PIXI.Graphics();
-        rect.beginFill(0xFFFF00);
-        rect.drawRect(this.x, this.y, scl, scl);
 
-    this.x = 0;
-    this.y = 0;
+function creatMatrix () {
+    for (var i = 0; i < 40; i++) {
+        arr[i] = new Array();
+        for (var j = 0; j < 40; j++) {
+            const pixes = new PIXI.Graphics();
+            pixes.beginFill(0xFFFF00);
+            pixes.drawRect(0, 0, scl, scl);
+            arr[i][j] = pixes
+            arr[i][j].x= i*scl;
+            arr[i][j].y= j*scl;
+            //app.stage.addChild( arr[i][j]);
+        }
+    }
+}
+
+function TailSegment(){
+    this.currentX=0 ;
+    this.currentY=0 ;
     this.xspeed = 1;
     this.yspeed = 0;
-    this.total = 1;
-    this.dir = function(x,y){
+
+    this.nextSection=  {x:0,y:0}
+    this.perciousSegment = {x:0,y:0}
+
+}
+function SnakeSegment(){
+    total++;
+    this.currentX = 0;
+    this.currentY = 0;
+    this.xspeed = 1;
+    this.yspeed = 0;
+
+   this.nextSection={x:0,y:0}
+   this.perciousSegment = {x:0,y:0}
+
+   this.goTo = function(){
+       this.perciousSegment.x = this.currentX;
+       this.perciousSegment.y = this.currentY;
+        this.nextSection.x = this.currentX=this.currentX  + this.xspeed;
+       this.nextSection.y = this.currentY=this.currentY  + this.yspeed;
+        if(this.nextSection.x == foodx && this.nextSection.y ==foody ){
+           removeMeal();
+           placemeal();
+           tailSegment = new TailSegment();
+           tailSegment.currentX = this.currentX;
+           tailSegment.currentY = this.currentY;
+            tailArr.push(tailSegment);
+            total++;
+            app.stage.addChild(arr[tailArr[total-1].currentX][tailArr[total-1].currentY])
+        }
+    app.stage.addChild(arr[this.nextSection.x][this.nextSection.y])
+    app.stage.removeChild(arr[this.currentX - this.xspeed][this.currentY-this.yspeed])
+   }
+
+
+    this.dir = function (x,y)
+    {
         this.xspeed = x;
         this.yspeed = y;
     }
-    this.init = function () {
-        tail.push(rect);
-        app.stage.addChild(rect);
-    }
-    this.addTail= function (){
-        const pixes = new PIXI.Graphics();
-        pixes.beginFill(0xFFFF00);
-        pixes.drawRect(this.x, this.y, scl, scl);
-        tail.push(pixes)
-        app.stage.addChild(pixes);
-    }
+}
 
-    this.eat = function (food){
-        var ab = rect.getBounds();
-        var bb = food.getBounds();
-        return ab.x + ab.width > bb.x && ab.x < bb.x + bb.width && ab.y + ab.height > bb.y && ab.y < bb.y + bb.height;
-
-    }
-
-    this.update = function () {
-            if (this.x >= 0 && this.x < window.innerWidth - scl) {
-                tail[0].x = this.x = this.x + this.xspeed * scl;
-            }
-            if (this.y >= 0 && this.y < window.innerHeight - scl) {
-            if(this.total>1){                                   //if more than 1 pieces
-                for(let i=1; i<tail.length; i++ ){
-                    tail[i].y = tail[i-1].y;                   //should replace all snake pieces on one step forward
-                    tail[i].x = tail[i-1].x;
-                    console.log("Trail.Y", tail[1].y);
-                }
-            }
-            tail[0].y = this.y = this.y + this.yspeed * scl; // move head snake one step forward
-            console.log("Head.Y",tail[0].y);
-            }
-    }
-};
 var handler = function (event) {
     if ([32, 37, 38, 39, 40].indexOf(event.keyCode) > -1) {
         event.preventDefault();
     }
     if (event.keyCode == 37) { // LEFT
-       snake.dir(-1,0);
+        S.dir(-1,0);
     }
     else if (event.keyCode == 39) { // RIGHT
-        snake.dir(1,0);
+        S.dir(1,0);
     } else if (event.keyCode == 38) { // UP
-        snake.dir(0,-1);
+        S.dir(0,-1);
     } else if (event.keyCode == 40) { // DOWN
-        snake.dir(0,1);
+        S.dir(0,1);
     }
 }
-var onResize = function (event) {
-   //app.renderer.resize(window.innerWidth, window.innerHeight);
-};
-
 function placemeal(){
     food = new PIXI.Graphics();
     food.beginFill(0x7FFF00);
-    food.drawRect(randomInt(5, this.app.renderer.view.width) - scl, randomInt(5, this.app.renderer.view.height), scl, scl);
+    foodx = randomInt(0, 40) ;
+    foody = randomInt(0, 40) ;;
+    food.drawRect(arr[foodx][foody].x, arr[foodx][foody].y, scl, scl);
+    arr[foodx][foody] = food;
     app.stage.addChild(food);
 
 }
-onResize();
+
+function removeMeal(){
+    app.stage.removeChild(arr[foodx][foody]);
+}
+
 function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-
-
